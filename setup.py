a73db8be871fe77740598ce664a02379dc7e9ee1
krystalgamer <callofdutypsp@gmail.com>
2022-02-20 01:17:54 +0000

    First version

--- setup.py
+++ setup.py
@@ -3,12 +3,17 @@ from setuptools import setup, find_packages
 
 setup(
         name='krystal',
-        version='0.0.0',
+        version='1.0.0',
         description="Krystal's static website builder",
         author='krystalgamer',
         author_email='krystalgamer@protonmail.com',
         url='https://github.com/krystalgamer/krystal',
         packages=find_packages(),
+		install_requires=[
+			'Flask>=2.0.3',
+			'Frozen-Flask>=0.18',
+			'marko>=1.2.0',
+		],
         entry_points={
             'console_scripts': [
                 'krystal = krystal.main:main'
