2c3ad26b0c5d3381cdff6b87d441bbe5b7d8650d
krystalgamer <callofdutypsp@gmail.com>
2023-11-02 22:38:38 +0100

    Freeze all packages to avoid similar situation in the future

--- setup.py
+++ setup.py
@@ -3,17 +3,18 @@ from setuptools import setup, find_packages
 
 setup(
         name='krystal',
-        version='2.0.0',
+        version='2.0.1',
         description="Krystal's static website builder",
         author='krystalgamer',
         author_email='krystalgamer@protonmail.com',
         url='https://github.com/krystalgamer/krystal',
         packages=find_packages(),
 		install_requires=[
-			'Flask>=2.0.3',
-			'Frozen-Flask>=0.18',
-			'marko>=2.0.1',
-			'Pygments>=2.11.2'
+			'Flask>=2.0.3, <3',
+			'Werkzeug<3',
+			'Frozen-Flask==0.18',
+			'marko==2.0.1',
+			'Pygments==2.16.1'
 		],
         entry_points={
             'console_scripts': [
