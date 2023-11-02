f59a71406167f2de76770cf5acba114a3f08683a
krystalgamer <callofdutypsp@gmail.com>
2023-11-02 22:14:44 +0100

    updated deps

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='krystal',
-        version='1.0.3',
+        version='2.0.0',
         description="Krystal's static website builder",
         author='krystalgamer',
         author_email='krystalgamer@protonmail.com',
@@ -12,7 +12,7 @@ setup(
 		install_requires=[
 			'Flask>=2.0.3',
 			'Frozen-Flask>=0.18',
-			'marko>=1.2.0',
+			'marko>=2.0.1',
 			'Pygments>=2.11.2'
 		],
         entry_points={
